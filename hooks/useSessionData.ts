188ccfff38f3fef66bf3bffe7c50bb18e1fef81a
KONATE BAKARY <69125430+Boukymen@users.noreply.github.com>
2024-11-03 21:13:31 +0000

    update all project to tsx

---
+++ hooks/useSessionData.ts
@@ -0,0 +1,23 @@
+import { Configuration, FrontendApi, Session } from "@ory/client";
+import { edgeConfig } from "@ory/integrations/next";
+import { useEffect, useState } from "react";
+
+const ory = new FrontendApi(new Configuration(edgeConfig));
+
+export const useSessionData = () => {
+  const [sessionData, setSessionData] = useState<Session | null>(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    ory
+      .toSession()
+      .then(({ data }) => {
+        setSessionData(data);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }, []);
+
+  return { sessionData, error };
+};
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    // / experimental: {
+    //     serverActions: true,
+    // },
+    reactStrictMode: false,
+    swcMinify: true,
+    images: {
+        domains: [
+            "public.blob.vercel-storage.com",
+        ],
+    },
+    redirects: async () => {
+        return [
+            {
+                source: "/",
+                destination: "/dashboard",
+                permanent: true,
+            },
+        ];
+    },
+
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
 };
 
 export default nextConfig;
